9d1a755d2ab558b6a410b75697a5da8c4a2d9322
LinlinLiang0 <llia618@aucklanduni.ac.nz>
2024-01-27 23:31:22 +1300

    add about me, projects, contact me section

--- MyPortfolioProject/app.js
+++ MyPortfolioProject/app.js
@@ -1,11 +1,6 @@
-// app.js
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, Express!');
-// });
 
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
